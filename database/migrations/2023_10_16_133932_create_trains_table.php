0c3f938dc1e1e1f1a7581c3f2d314f3e806554cf
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-16 17:39:45 +0200

    fix precision columns time

--- database/migrations/2023_10_16_133932_create_trains_table.php
+++ database/migrations/2023_10_16_133932_create_trains_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('company', 50);
             $table->string('departure_station', 50);
             $table->string('arrival_station', 50);
-            $table->time('departure_time');
-            $table->time('arrival_time');
+            $table->time('departure_time', $precision = 0);
+            $table->time('arrival_time', $precision = 0);
             $table->string('code_train', 50);
             $table->tinyInteger('carriage');
             $table->boolean('in_time');
