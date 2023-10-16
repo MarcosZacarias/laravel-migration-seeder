a6f6d89d29e55e4c19d6b2619b54b3af9e8e8e84
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-16 17:19:29 +0200

    add route train index

---
+++ resources/views/train/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('main-content')
+  <section class="container mt-5">
+    <div class="container">
+        <div class="row  g-4">
+            @forelse($trains as $train)
+            <div class="col">              
+                <div class="card">
+                  <div class="card-body">
+                    <h3 class="card-title"><strong>{{$train->company}}</strong></h3>
+                    <p class="card-text">
+                        <ul>
+                            <li><strong>Stazione di arrivo: </strong>{{$train->code_train}}</li>
+                            <li><strong>Stazione di partenza: </strong>{{$train->departure_station}}</li>
+                            <li><strong>Stazione di arrivo: </strong>{{$train->arrival_station}}</li>
+                            <li><strong>Stazione di arrivo: </strong>{{$train->departure_time}}</li>
+                            <li><strong>Stazione di arrivo: </strong>{{$train->arrival_time}}</li>
+                            <li><strong>Stazione di arrivo: </strong>{{$train->carriage}}</li>
+                            <li><strong>Stazione di arrivo: </strong>{{$train->in_time}}</li>
+                            <li><strong>Stazione di arrivo: </strong>{{$train->deleted}}</li>
+
+                        </ul>
+                    </p>
+                  </div>
+                </div>              
+            </div>
+            @empty
+            <h2>Non ci sono risultati</h2>
+            @endforelse
+        </div>
+    </div>
+  </section>
+@endsection
